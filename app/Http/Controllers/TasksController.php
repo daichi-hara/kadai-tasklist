9c094abeaeaa0f3fc522e43fa8e487c3406e69be
daichi.hara <hara@lassic.co.jp>
2020-04-29 12:21:23 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,7 +46,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -95,7 +98,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
